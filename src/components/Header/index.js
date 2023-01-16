06e81413cfe757a2fe4f2c9d4ed0d4269ed4560b
Gabriel <gabriel.alves.rosario@gmail.com>
2023-01-16 13:35:15 +0000

    fist commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as C from "./style"
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Title>
+        <C.Header>Controle Financeiro</C.Header>
+      </C.Title>
+    </C.Container>
+  )
+}
+
+export default Header
